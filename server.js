2a0d31a6bed52b5393965230d4fdec226875d786
tornike <tornike.tanda@gmail.com>
2023-11-29 06:48:29 -0800

    first commit

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(cookieParser());
 // app.use(cors());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://recipereact.onrender.com",
     credentials: true, // Enable credentials (cookies)
   })
 );

c17fd923a0d1d2cdee08a209a0dd014d3a9c4581
Your Name <your.email@example.com>
2023-12-01 11:02:20 -0800

    first commit

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(
   cors({
     // origin: "https://recipereact.onrender.com",
-    origin: "http://localhost:3000",
+    origin: "https://mernclient-v70c.onrender.com/",
     credentials: true, // Enable credentials (cookies)
   })
 );
